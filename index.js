7ba330f3ba352fa4a0711699ec5d9ecb33f44436
Anik <aniksarker1612@gmail.com>
2020-09-26 15:52:13 +0800

    get and post conceptual code

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('hi, there!')
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({
+        fruit: 'banana',
+        quantity: 1000,
+        price: '1000$'
+    })
 })
+
+users = ['asad', 'saber', 'susmita', 'sohana', 'kolim', 'alomgir'];
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({name, id});
+})
+
+// post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user)
+})
+
 
 app.listen(8080, () => console.log('listening to port 8080'));
